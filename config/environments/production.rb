6b720c1a0a1c7ed85ceea7ca21ac6336fd8a9f32
AngieChilds <pirateprincess_avh@yahoo.com>
2014-05-27 17:50:15 +0200

    list id

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Wec2::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
-
+config.action_mailer.perform_deliveries = true
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.smtp_settings = {
