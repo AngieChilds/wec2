bb647a8839816e5ff34b3c6fdc429b2f7156eb78
AngieChilds <pirateprincess_avh@yahoo.com>
2014-05-27 19:00:15 +0200

    api envir.rb

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -4,7 +4,7 @@ column :email, :string
 validates_presence_of :email
 validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 def subscribe
-
+mailchimp = Gibbon::API.new("7c304b2da2bdb8123d4256a5c62d1812-us8")
 result = mailchimp.lists.subscribe({
 :id => "aeca627dea",
 :email => {:email => self.email},
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Wec2::Application.initialize!
-mailchimp = Gibbon::API.new("7c304b2da2bdb8123d4256a5c62d1812-us8")
