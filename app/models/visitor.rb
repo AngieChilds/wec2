d48a3b4d80162a175069da96d756692d305fb3c7
AngieChilds <pirateprincess_avh@yahoo.com>
2014-05-27 18:34:37 +0200

    api list id

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -4,9 +4,9 @@ column :email, :string
 validates_presence_of :email
 validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 def subscribe
-mailchimp = Gibbon::API.new
+mailchimp = Gibbon::API.new("7c304b2da2bdb8123d4256a5c62d1812-us8")
 result = mailchimp.lists.subscribe({
-:id => ENV['MAILCHIMP_LIST_ID'],
+:id => ENV['aeca627dea'],
 :email => {:email => self.email},
 :double_optin => false,
 :update_existing => true,
